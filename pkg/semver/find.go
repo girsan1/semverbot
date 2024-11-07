04e47d625531a06a08eeb4ad823e8382e5b2fc37
alex.giria <aliaksandr.giria@nike.com>
2024-11-07 15:13:31 +0200

    Add support for projects that use the monorepo approach #82

--- pkg/semver/find.go
+++ pkg/semver/find.go
@@ -2,9 +2,9 @@ package semver
 
 import (
 	"fmt"
-	"strings"
-
 	blangsemver "github.com/blang/semver/v4"
+	"regexp"
+	"strings"
 )
 
 // Find finds the biggest valid semver version in a slice of strings.
@@ -14,7 +14,15 @@ func Find(prefix string, suffix string, versions []string) (found string, err er
 	var parsedVersions blangsemver.Versions
 	var parsedVersion blangsemver.Version
 
+	var validVersion = regexp.MustCompile(`^\d+\.\d+\.\d+$`) //to allow simple version like 0.23.234
+	var filteredVersions []string
 	for _, version := range versions {
+		if validVersion.MatchString(version) || strings.Contains(version, prefix) && strings.Contains(version, suffix) {
+			filteredVersions = append(filteredVersions, version)
+		}
+	}
+
+	for _, version := range filteredVersions {
 		if parsedVersion, err = Parse(prefix, suffix, version); err != nil {
 			continue
 		}
@@ -31,7 +39,7 @@ func Find(prefix string, suffix string, versions []string) (found string, err er
 	var targetVersion = parsedVersions[len(parsedVersions)-1]
 
 	// necessary because blangsemver's Version.String() strips any prefix
-	for _, version := range versions {
+	for _, version := range filteredVersions {
 		if strings.Contains(version, targetVersion.String()) {
 			found = version
 			break
